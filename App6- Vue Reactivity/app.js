b6a3f8998d32cdf248b0447d56ef9be80eb636a9
Stan Mandela <smstan8@gmail.com>
2022-05-31 22:23:15 +0300

    New addition to the list

--- App6- Vue Reactivity/app.js
+++ App6- Vue Reactivity/app.js
@@ -10,9 +10,14 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      //this.message = this.currentUserInput;
+     // console.log(this.$refs.userText.value);
+     this.message= this.$refs.userText.value;
+ 
     },
   },
 });
 
 app.mount('#app');
+
+//''''Second app'''''''''
